bc38d65f8db69a687243a243b8c1f1c3b45a53cb
JkKU <60291632+Jenrikku@users.noreply.github.com>
2022-05-01 12:34:38 +0200

    Create Extensions.cs

---
+++ CGFXLeaf/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Syroot.BinaryData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGFXLeaf {
+    internal static class Extensions {
+        public static bool[] ReadBits(this BinaryDataReader reader, int byteAmount) {
+            foreach(byte bits in reader.ReadBytes(byteAmount)) {
+                //bits & 0b_1000_0000;
+            }
+        }
+    }
+}
