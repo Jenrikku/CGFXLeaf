7f823829fe97fd23c592af4613b51faca3277ebb
JkKU <60291632+Jenrikku@users.noreply.github.com>
2022-05-01 20:53:38 +0200

    Creates project from template

---
+++ ModelViewer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ModelViewer {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using(var game = new GameController())
+                game.Run();
+        }
+    }
+}
