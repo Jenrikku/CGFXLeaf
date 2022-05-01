70bcac1c0200639bf82c1ce82b977614ec8b862e
Trippixyz <69848127+Trippixyz@users.noreply.github.com>
2022-05-01 12:42:36 +0200

    Update Extensions.cs

--- CGFXLeaf/Extensions.cs
+++ CGFXLeaf/Extensions.cs
@@ -7,9 +7,18 @@ using System.Threading.Tasks;
 
 namespace CGFXLeaf {
     internal static class Extensions {
+        static byte[] bitArray = new byte[] { 0x01, 0x02, 0x04,0x08, 0x10, 0x20, 0x40, 0x80 };
         public static bool[] ReadBits(this BinaryDataReader reader, int byteAmount) {
-            foreach(byte bits in reader.ReadBytes(byteAmount)) {
-                //bits & 0b_1000_0000;
+            byte[] bytes = reader.ReadBytes(byteAmount);
+            bool[] bits = new bool[byteAmount * 8];
+            for (int j = 0; j < bytes.Length; j++) {
+                for (int i = 0; i < 8; i++)
+                {
+                    //if (bytes[j] & bitArray[j])
+                    {
+
+                    }
+                }
             }
         }
     }
